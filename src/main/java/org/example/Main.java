24522bd2678c125312241e05496e742c10d5233f
Serian Aklunc <57585672+AniKrayMe@users.noreply.github.com>
2023-05-24 23:51:13 +0300

    v2.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        calc("VII+X+X");
+
+    }
+
+    public static void calc(String exp) throws Exception {
+
         Converter converter = new Converter();
         String[] actions = {"+", "-", "/", "*"};
-        String[] regexActions = {"\\+", "-", "/", "\\*"};   //Этот масив я создал для строки номер 26, нужен для split
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Введите выражение: ");
-        String exp = scn.nextLine();
+        String[] regexActions = {"\\+", "-", "/", "\\*"};  //Этот масив я создал для строки номер 26, нужен для split
         int actionIndex = -1;
         for (int i = 0; i < actions.length; i++) {
             if (exp.contains(actions[i])) {
@@ -19,27 +23,26 @@ public class Main {
             }
         }
         if (actionIndex == -1) {
-            System.out.println("Некорректное выражение");
-            return;
+            throw new Exception("Некорректное выражение");
         }
 
         String[] data = exp.split(regexActions[actionIndex]);
-        if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1]) && data.length == 2) {
             int a, b;
-                boolean isRoman = converter.isRoman(data[0]);
+            boolean isRoman = converter.isRoman(data[0]);
             if (isRoman) {
                 a = converter.romanToInt(data[0]);
                 b = converter.romanToInt(data[1]);
-                if (a > 10 || b > 10){
-                    System.out.println("только числа от 1 до 10-и");
-                    return;
+
+
+                if (a > 10 || b > 10) {
+                    throw new Exception("только числа от 1 до 10-и");
                 }
             } else {
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
-                if (a > 10 || b > 10){
-                    System.out.println("только числа от 1 до 10-и");
-                    return;
+                if (a > 10 || b > 10) {
+                    throw new Exception("только числа от 1 до 10-и");
                 }
             }
             int result;
@@ -64,8 +67,12 @@ public class Main {
                 System.out.println(result);
             }
         } else {
-            System.out.println("Числа должны быть в одном формате");
+            throw new Exception("incorrect input");
         }
+
     }
+
 }
+
+
 
